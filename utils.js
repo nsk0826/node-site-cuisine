c458bd6bbe93b121ec0a0d560c4bd7d6826b8cb6
nsk0826 <nsk.20160401@gmail.com>
2022-07-28 22:49:08 +0900

    add utils

--- utils.js
+++ utils.js
@@ -1,3 +1,16 @@
 const fs = require("fs"),
-httpStatus = require("http-status-codes"),
-contentTypes = require("./contentTypes");
+  httpStatus = require("http-status-codes"),
+  contentTypes = require("./contentTypes");
+
+// ファイルを読んでレスポンスを返す
+module.exports = {
+  getFile: (file, res) => {
+    fs.readFile(`./${file}`, (error, data) => {
+      if (error) {
+        res.writeHead(httpStatus.StatusCodes.INTERNAL_SERVER_ERROR, contentTypes.html);
+        res.end("There was an error serving content!")
+      }
+      res.end(data)
+    });
+  }
+}
