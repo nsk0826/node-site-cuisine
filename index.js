9f13026185ba66ed003d62478a28725da133cb88
nsk0826 <nsk.20160401@gmail.com>
2022-07-28 22:20:50 +0900

    add index

---
+++ index.js
@@ -0,0 +1,7 @@
+const port = 3000,
+
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  router = require("./roter"),
+  contentType = require("./contentTypes"),
+  utils = require("./utils");
